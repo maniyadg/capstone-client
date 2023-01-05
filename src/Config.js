57d309d39947fa550c6752ab7f22726068b31692
maniyadg <maniyarasandhanasekaran@gmail.com>
2023-01-05 15:13:22 +0530

    api added

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,4 @@
 export const config = {
-    api : "http://localhost:3001"
+    // api : "http://localhost:3001",
+    api:"https://dashboard.render.com/web/srv-cer9fk2rrk00n42fd6f0/deploys/dep-cer9jrsgqg486p7aob9g"
 }
